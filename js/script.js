7f67ff2d3e785c2c121c658f7a8347616296c6be
JoshPhronesis <akosajoshua@yahoo.com>
2017-07-15 13:36:30 +0300

    Add reset functionalities

--- js/script.js
+++ js/script.js
@@ -7,6 +7,18 @@ var h1 = document.querySelector("h1");
 
 var messageDisplay = document.querySelector("#message");
 
+var resetButton = document.querySelector("#reset");
+
+resetButton.addEventListener("click",function(){
+    colors=generateRandomColors(6);
+    pickedColor = pickColor();
+    colorDisplay.textContent=pickedColor;
+    for(var i=0;i<squares.length;i++){
+        squares[i].style.background=colors[i];
+    }
+    h1.style.background="#232323";
+})
+
 colorDisplay.textContent=pickedColor;
 
 for(var i=0; i<squares.length;i++){
@@ -14,6 +26,7 @@ for(var i=0; i<squares.length;i++){
     squares[i].addEventListener("click",function(){
         var clickedColor = (this.style.background);
         if(clickedColor===pickedColor){
+            resetButton.textContent="Play again ?";
             messageDisplay.textContent="Correct";
             changeColors(clickedColor);
             h1.style.background = clickedColor;
