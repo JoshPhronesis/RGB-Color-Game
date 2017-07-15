6fde52ed16d5c6c48c7d155b21afe29560cca84c
JoshPhronesis <akosajoshua@yahoo.com>
2017-07-15 13:22:17 +0300

    Make colors random

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
-var colors =['red','green','blue','white','yellow','cyan'];
+var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll(".square");
-var pickedColor =randomColor();
+var pickedColor = pickColor();
 var colorDisplay = document.querySelector("#colorDisplay");
+var h1 = document.querySelector("h1");
+
 var messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent=pickedColor;
@@ -14,11 +16,27 @@ for(var i=0; i<squares.length;i++){
         if(clickedColor===pickedColor){
             messageDisplay.textContent="Correct";
             changeColors(clickedColor);
+            h1.style.background = clickedColor;
      }else{
             this.style.background="#232323";
             messageDisplay.textContent="Try Again";
         }
     })
+}
+
+function generateRandomColors(num){
+    var colorArr = [];
+    for(var i=0; i<num;i++){
+        colorArr.push(randomColors());
+    }
+    return colorArr;
+}
+
+function randomColors(){
+    var r = Math.floor(Math.random()*256);
+    var g = Math.floor(Math.random()*256);
+    var b = Math.floor(Math.random()*256);
+    return "rgb("+r+", "+g+", "+b+")";
 }
 
 function changeColors(color){
@@ -27,7 +45,7 @@ function changeColors(color){
     }
 }
 
-function randomColor(){
+function pickColor(){
     var randNum = Math.floor(Math.random()*colors.length);
     return colors[randNum];
 }
